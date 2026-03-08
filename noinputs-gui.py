5ca7eae56671b91f92efc9cbc70bc3b27c72f4ac
paul.veles <bellevillepaul03@gmail.com>
2026-03-08 21:42:24 +0100

    now shows the status of each device in the list view

--- noinputs-gui.py
+++ noinputs-gui.py
@@ -1,9 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QAbstractItemView, QHeaderView
 from PyQt6 import uic
-from PyQt6.QtCore import QStringListModel
+from PyQt6.QtCore import QStringListModel, Qt
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
-import noinputs
 import devicemanager
 
 
@@ -16,7 +15,18 @@ class MainWindow(QMainWindow):
         uic.loadUi("mainwindow.ui", self)
 
         self.model = QStandardItemModel()
-        self.listView.setModel(self.model)
+        self.model.setHorizontalHeaderLabels(["Name", "Status"])
+        self.deviceTableView.setShowGrid(False)
+        self.deviceTableView.verticalHeader().hide()
+        self.deviceTableView.horizontalHeader().hide()
+        self.deviceTableView.setAlternatingRowColors(True)
+        self.deviceTableView.setModel(self.model)
+        self.deviceTableView.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+        # after setting your model on the table view
+        self.deviceTableView.horizontalHeader().setStretchLastSection(False)  # last column doesn't stretch
+        self.deviceTableView.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)  # first column stretches
+        self.deviceTableView.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)  # status column resizes to content
+
         self.load_inputs()
 
         self.inhibitButton.clicked.connect(self.inhibit_selected_device)
@@ -25,22 +35,40 @@ class MainWindow(QMainWindow):
     def load_inputs(self):
         devices_list = self.device_manager.devices
         for d in devices_list:
-            item = QStandardItem(d.name)
-            self.model.appendRow(item)
+            name_item = QStandardItem(d.name)
+            # Status column
+            status_text = "Disabled" if d.get_inhibit() else "Enabled"
+            status_item = QStandardItem(status_text)
+            # Align status to right
+            status_item.setTextAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+            self.model.appendRow([name_item, status_item])
 
     def inhibit_selected_device(self):
-        selected_indexes = self.listView.selectionModel().selectedIndexes()
+        selected_indexes = self.deviceTableView.selectionModel().selectedIndexes()
         if selected_indexes:
             listview_index = selected_indexes[0]
             index = listview_index.row()
             self.device_manager.inhibit_device(index)
+            self.update_status(index)
 
     def unhibit_selected_device(self):
-        selected_indexes = self.listView.selectionModel().selectedIndexes()
+        selected_indexes = self.deviceTableView.selectionModel().selectedIndexes()
         if selected_indexes:
             listview_index = selected_indexes[0]
             index = listview_index.row()
             self.device_manager.uninhibit_device(index)
+            self.update_status(index)
+
+    def update_status(self, row:int):
+        device = self.device_manager.devices[row]
+        status = device.get_inhibit()
+        status_item = self.model.item(row,1)
+        if status:
+            status_item.setText("Disabled")
+        else:
+            status_item.setText("Enabled")
+
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
